75737e550d8c84bba11d9d095fc3eecdf533114e
BruceLeeK <kanadeanurag87@gmail.com>
2023-08-20 11:12:46 +0530

    20/8/23 11:12

--- lib/view/screen/splash_screen.dart
+++ lib/view/screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:cart_app/view/screen/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -8,10 +9,20 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      splash: Image.network(
-        "https://i.pinimg.com/564x/ab/20/4c/ab204ca61e46787016bc3b787644a4f6.jpg",
-        fit: BoxFit.cover,
-        height: 800,
+      splash: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              "Buy Your choice",
+              style: GoogleFonts.greatVibes(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+            //Great Vibes, Dancing Script, Sofia, Pacifico, Meow Script,
+          ],
+        ),
       ),
       nextScreen: HomePage(),
       duration: 40000,
